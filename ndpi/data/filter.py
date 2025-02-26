941c085282d618e4a609781b17b2f5ca2f43b594
jmuecke <48099698+jmuecke@users.noreply.github.com>
2025-02-26 15:16:56 +0100

    Add generic filter function

---
+++ ndpi/data/filter.py
@@ -0,0 +1,46 @@
+from numpy import e
+import polars as pl
+from typing import Any, Callable, Tuple, Union
+
+
+def filter(
+    df, filter: Union[pl.Expr, Callable], name: str, id_col="id"
+) -> Tuple[pl.DataFrame, pl.DataFrame]:
+    """Common filtering API. Returns filtered dataframe and metadata on the filtered IDs.
+
+    Args:
+        df: input dataframe
+        id_col: column with unique value
+        filter: polars expression or filter function that operates on a dataframe
+        name: label for the filtering step
+
+    Returns:
+        Tuple with filtered lazy dataframe and metadata
+    """
+    # Cast to lazy
+    df = df.lazy()
+
+    len_in = df.select(pl.len()).collect()
+    ids = df.select(id_col)
+
+    if isinstance(filter, pl.Expr):
+        df_out = df.filter(filter)
+    else:
+        df_out = filter(df)
+
+    len_out = df_out.select(pl.len()).collect()
+    filtered = (
+        (ids.filter(~pl.col(id_col).is_in(df_out.select(id_col))))
+        .select(id_col)
+        .collect()
+    )
+
+    stat = {
+        "name": name,
+        "before": len_in,
+        "after": len_out,
+        "reduction": len_in - len_out,
+        "ids": [filtered],
+    }
+
+    return df_out, pl.DataFrame(stat)
